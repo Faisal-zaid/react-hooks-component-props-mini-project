8b1d001738b89ceda5064ca7c39064ef33ada83e
Faisal Zaid <zaidalideveloper@gmail.com>
2025-10-18 12:39:57 +0300

    final commit

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Article({title, date="January 1, 1970", preview, minutes}) {
+    function matime(minutes){
+        if(minutes<30){
+            const cups=Math.ceil(minutes/5);
+            return"☕️".repeat(cups);
+        }else{
+            const bentos=Math.ceil(minutes/10);
+            return "🍱".repeat(bentos);
+        }
+    }
+  return (
+    <article>
+        <h3>{title}</h3>
+        <small>{date}</small>
+        <p>{matime(minutes)} {minutes} min read</p>
+        <p>{preview}</p>
+    </article>
+  )
+}
